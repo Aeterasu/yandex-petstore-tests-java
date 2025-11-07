9fa722ef2ee024033c0b1fe8db0299a8f5df0ded
Matthew <matveykerov@gmail.com>
2025-11-07 22:43:03 +0300

    Initial commit

---
+++ src/main/java/org/aeterasu/petstore/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.aeterasu.petstore;
+
+/**
+ *
+ * @author Lain
+ */
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
