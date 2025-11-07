029341e9414a9f5d1e1ebb16ba18ac8d181b95ab
Matthew <matveykerov@gmail.com>
2025-11-07 23:44:49 +0300

    Basic get test

--- src/test/java/org/aeterasu/petstore/PetTests.java
+++ src/test/java/org/aeterasu/petstore/PetTests.java
@@ -1,6 +1,29 @@
 package org.aeterasu.petstore;
 
+import java.net.URI; 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
 public class PetTests
 {
-    
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 0, 1, 5, 15, Integer.MAX_VALUE})
+	void getPetTest(int id) throws Exception
+	{
+		HttpRequest request = HttpRequest.newBuilder()
+			.uri(URI.create(Api.BASE_URL + "/pet/" + Integer.toString(id)))
+			.GET()
+			.build();
+
+		HttpClient httpClient = HttpClient.newHttpClient();
+		HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+
+		assertEquals(response.statusCode(), 200);
+	}
 }
